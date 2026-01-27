a84ed85423752dab0ec090491ed85fe202fd35c1
Katie Brown <kbrown@pointblue.org>
2026-01-27 16:34:14 -0500

    Update tests

--- tests/src/FunctionalJavascript/PracticeReportFormTest.php
+++ tests/src/FunctionalJavascript/PracticeReportFormTest.php
@@ -14,6 +14,7 @@ class PracticeReportFormTest extends RcdTestBase {
    */
   public function testPracticeReportForm() {
     $plan_storage = \Drupal::entityTypeManager()->getStorage('plan');
+    $org_storage = \Drupal::entityTypeManager()->getStorage('organization');
 
     // Confirm that the practice report form is accessible.
     $this->drupalGet('/report/practices');
@@ -30,18 +31,30 @@ class PracticeReportFormTest extends RcdTestBase {
     $this->assertSession()->pageTextNotContains('Practices: ');
     $this->assertSession()->pageTextNotContains('Status of implementations: ');
 
+    // Create a farm organization.
+    /** @var \Drupal\organization\Entity\OrganizationInterface $farm */
+    $farm = $org_storage->create([
+      'type' => 'farm',
+      'name' => $this->randomMachineName(),
+    ]);
+    $farm->save();
+
     // Create two practice implementation plans.
     $plan1 = $plan_storage->create([
       'type' => 'rcd_practice_implementation',
+      'farm' => [$farm],
       'rcd_practice' => 'cover_crop',
+      'rcd_acres' => 8,
       'name' => $this->randomMachineName(),
       'status' => 'planning',
     ]);
     $plan1->save();
     $plan2 = $plan_storage->create([
       'type' => 'rcd_practice_implementation',
+      'farm' => [$farm],
       'rcd_practice' => 'other',
       'rcd_practice_other' => 'Bioremediation',
+      'rcd_linear_feet' => 250,
       'name' => $this->randomMachineName(),
       'status' => 'implementing',
     ]);
@@ -55,6 +68,10 @@ class PracticeReportFormTest extends RcdTestBase {
     $this->assertSession()->pageTextContains('Generated: ' . date('Y-m-d h:i'));
     $this->assertSession()->pageTextContains('Practices: Cover Crop, Bioremediation');
     $this->assertSession()->pageTextContains('Status of implementations: Planning, Implementing');
+    $this->assertSession()->pageTextContains('Total implementation plans: 2');
+    $this->assertSession()->pageTextContains('Total farms: 1');
+    $this->assertSession()->pageTextContains('Total acreage: 8');
+    $this->assertSession()->pageTextContains('Total linear feet: 250');
 
     // Test filtering by practice.
     $this->drupalGet('/report/practices');
@@ -63,6 +80,10 @@ class PracticeReportFormTest extends RcdTestBase {
     $this->assertTrue($this->assertSession()->waitForText('1 practice implementation plans analyzed.', 30000));
     $this->assertSession()->pageTextContains('Practices: Cover Crop');
     $this->assertSession()->pageTextContains('Status of implementations: Planning');
+    $this->assertSession()->pageTextContains('Total implementation plans: 1');
+    $this->assertSession()->pageTextContains('Total farms: 1');
+    $this->assertSession()->pageTextContains('Total acreage: 8');
+    $this->assertSession()->pageTextNotContains('Total linear feet: ');
 
     // Test filtering by plan status.
     $this->drupalGet('/report/practices');
@@ -71,6 +92,10 @@ class PracticeReportFormTest extends RcdTestBase {
     $this->assertTrue($this->assertSession()->waitForText('1 practice implementation plans analyzed.', 30000));
     $this->assertSession()->pageTextContains('Practices: Bioremediation');
     $this->assertSession()->pageTextContains('Status of implementations: Implementing');
+    $this->assertSession()->pageTextContains('Total implementation plans: 1');
+    $this->assertSession()->pageTextContains('Total farms: 1');
+    $this->assertSession()->pageTextContains('Total linear feet: 250');
+    $this->assertSession()->pageTextNotContains('Total acreage: ');
   }
 
 }
